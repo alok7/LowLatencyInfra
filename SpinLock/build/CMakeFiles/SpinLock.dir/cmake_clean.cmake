a780c07c1050e6041a9cce5546020f6f1562ec7d
Alok Diwakar <alokdiwakar@aloks-mbp.mynet>
2022-08-21 18:50:24 +0800

    adding spinlock

---
+++ SpinLock/build/CMakeFiles/SpinLock.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SpinLock.dir/main.cpp.o"
+  "CMakeFiles/SpinLock.dir/main.cpp.o.d"
+  "SpinLock"
+  "SpinLock.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SpinLock.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
